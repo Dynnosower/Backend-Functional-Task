21ee2de61ae0585727223358caa4bfefe39d4c49
Dynnosower <infantevillarin@gmail.com>
2024-08-27 18:56:21 +0800

    feat: Carrier endpoints - Create GetCarriers endpoint for getting all carriers - Create UpdateCarrier endpoint for enabling and disabling a specific carrier

--- CarrierRates.Api/Controllers/CarrierController.cs
+++ CarrierRates.Api/Controllers/CarrierController.cs
@@ -1,11 +1,14 @@
 using System.Diagnostics;
 using System.Text.Json;
+using CarrierRates.Api.Data;
 using CarrierRates.Api.Dtos.Dhl;
 using CarrierRates.Api.Dtos.Lalamove;
+using CarrierRates.Api.Entities;
 using CarrierRates.Api.Factories;
 using CarrierRates.Api.Mapping;
 using CarrierRates.Api.Strategies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarrierRates.Api.Controllers
 {
@@ -18,6 +21,40 @@ namespace CarrierRates.Api.Controllers
         public CarrierController(CarrierStrategyFactory factory)
         {
             _factory = factory;
+        }
+
+        [Consumes("application/json")]
+        [HttpGet("")]
+        public IActionResult GetCarriers(CarrierRatesContext dbContext)
+        {
+            var carrierList = dbContext.Carriers.ToList<Carrier>();
+            return Ok(carrierList);
+        }
+
+        [Consumes("application/json")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCarrier(int id, bool isEnabled, CarrierRatesContext dbContext)
+        {
+            var existingCarrier = await dbContext.Carriers.FindAsync(id);
+
+            if (existingCarrier is null)
+            {
+                return NotFound("Carrier does not exist.");
+            }
+
+            dbContext.Entry(existingCarrier)
+            .CurrentValues
+            .SetValues(new Carrier()
+            {
+                Id = existingCarrier.Id,
+                Name = existingCarrier.Name,
+                isEnabled = isEnabled,
+                UpdatedAt = DateOnly.FromDateTime(DateTime.Now)
+            });
+
+            await dbContext.SaveChangesAsync();
+
+            return Ok("Carrier updated.");
         }
 
         [Consumes("application/json")]
@@ -61,13 +98,6 @@ namespace CarrierRates.Api.Controllers
                 default:
                     return StatusCode(StatusCodes.Status500InternalServerError, "Invalid carrier");
             }
-        }
-
-        [Consumes("application/json")]
-        [HttpGet("rates")]
-        public async Task<IActionResult> GetCarriers()
-        {
-            return Ok();
         }
     }
 }
